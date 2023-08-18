19cf0393b32ec58b10ffed8c577e9e5a0c4fca71
Vbehal01 <vanshbehal01@gmail.com>
2023-08-18 15:37:01 +0530

    add role in token

--- auth.py
+++ auth.py
@@ -4,15 +4,17 @@ import os
 
 load_dotenv()
 
-SECRET = os.environ['secret']
-ALGORITHM = os.environ['algorithm']
-# print(SECRET)
+SECRET = os.environ["secret"]
+ALGORITHM = os.environ["algorithm"]
 
-def create_token(email: str):
-    return jwt.encode({
-        "email": email,
-        "project": "online judge"
-    }, SECRET, algorithm=ALGORITHM)
+
+def create_token(email: str, role: str):
+    return jwt.encode(
+        {"role": role, "email": email, "project": "online judge"},
+        SECRET,
+        algorithm=ALGORITHM,
+    )
+
 
 def decode_token(token: str):
-    return jwt.decode(token, SECRET,algorithms=[ALGORITHM])
+    return jwt.decode(token, SECRET, algorithms=[ALGORITHM])
