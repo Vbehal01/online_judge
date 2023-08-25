c98e07e513ee0717f1931bf577049862a3795e56
Vbehal01 <vanshbehal01@gmail.com>
2023-08-25 17:01:18 +0530

    add for loop to iterate between test cases and stopping the test case where the answer is wrong

--- backend/crud.py
+++ backend/crud.py
@@ -163,7 +163,7 @@ def get_submissions(db: Session):
 def get_submision_by_id(db: Session, submission_id: int):
     return db.query(model.Submission).filter(model.Submission.id == submission_id).first()
 
-def update_submission_status(submission: schema.Submission,test_case_id: int, id: int, db: Session):
+def update_submission_status_to_wrong(db:Session, id: int, test_case_id: int):
     submission = get_submision_by_id(db,id)
     
     if submission.status == "Accepted":
@@ -178,21 +178,24 @@ def update_submission_status(submission: schema.Submission,test_case_id: int, id
 
 def create_submission(db: Session, submission: schema.Submission, solver_id: str):
     db_submission = model.Submission(code=submission.code, solver_id=solver_id, language_id=submission.language_id, question_id=submission.question_id)
-    db_question=get_question_by_id(db, submission.question_id)
-    input=db_question.test_cases.input
-    print(input)
-    expected_output=db_question.test_cases.output
-    print(expected_output)
-    test_case_id=db_question.test_cases.id
-    url='http://127.0.0.1:8080/evaluation'
-    myobj={'code': submission.code, 'test_case_input': input}
-    eval=requests.post(url, json=myobj)
-    eval=eval.json()
     db.add(db_submission)
     db.commit()
     db.refresh(db_submission)
-    if(eval["output"]!=expected_output):
-        update_submission_status(db, id, test_case_id)
-        db.commit
+    db_question=get_question_by_id(db, submission.question_id)
+    for test_case in db_question.test_cases:
+        input=test_case.input
+        print(input)
+        expected_output=test_case.output
+        print(expected_output)
+        test_case_id=test_case.id
+        url='http://127.0.0.1:8080/evaluation'
+        myobj={'code': submission.code, 'test_case_input': input}
+        eval=requests.post(url, json=myobj)
+        eval=eval.json()
+        print(eval)
+        if(eval["output"]!=expected_output):
+            update_submission_status_to_wrong(db, db_submission.id, test_case_id)
+            db.commit()
+            break
 
     return db_submission
