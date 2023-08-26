9ac7c31a55a5356d96d3056d5159fe42f6ef64bc
Vbehal01 <vanshbehal01@gmail.com>
2023-08-26 15:55:34 +0530

    add port changing python command and passing the name of script file in the form of string to write the code in it

--- evaluation/main.py
+++ evaluation/main.py
@@ -17,30 +17,29 @@ class EvaluationCreate(BaseModel):
     class Config:
         orm_mode = True
 
-
 app=FastAPI()
 
 @app.post("/evaluation/")
 def evaluation(eval: EvaluationCreate):
-    script_filename = generate_unique_filename()
+    script_filename = f"{generate_unique_filename()}"
     with open(script_filename, 'w') as script_file:
         script_file.write(eval.code)
-    
-    script_path=os.path.abspath(script_filename).replace('\\','\\\\')
-    print(f"{script_path}")
 
-        # "D:\\Learning\\online_judge\\evaluation\\user_script.py"
+    script_path=os.path.abspath(script_filename).replace('\\','\\\\')
+        
     try:
-        print(eval.test_case_input)
-        output = subprocess.check_output(["echo", eval.test_case_input, "|", f"{script_path}"], stderr=subprocess.STDOUT, text=True, shell=True).rstrip('\n')
-        print(output)
+        output = subprocess.check_output(["echo", eval.test_case_input, "|", "python", f"{script_path}"], stderr=subprocess.STDOUT, text=True, shell=True).rstrip('\n')
         if output:
             return {"input":eval.test_case_input,
                     "output": output}
-        else :
+        else:
             raise HTTPException(status_code=500, detail="Error while executing code")
         
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
     finally:
         os.remove(script_filename)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
