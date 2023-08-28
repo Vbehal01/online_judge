c5c6e4f402ea59b940457037555ba796497df041
Vbehal01 <vanshbehal01@gmail.com>
2023-08-28 15:44:57 +0530

    adding try catch to check the reason of error'

--- evaluation/main.py
+++ evaluation/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, HTTPException
 import subprocess
 import os
 from datetime import datetime
-import uvicorn
 
 def generate_unique_filename():
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -19,27 +18,27 @@ class EvaluationCreate(BaseModel):
 
 app=FastAPI()
 
-@app.post("/evaluation/")
-def evaluation(eval: EvaluationCreate):
-    script_filename = f"{generate_unique_filename()}"
-    with open(script_filename, 'w') as script_file:
-        script_file.write(eval.code)
+try:
+    @app.post("/evaluation/")
+    def evaluation(eval: EvaluationCreate):
+        script_filename = f"{generate_unique_filename()}"
+        with open(script_filename, 'w') as script_file:
+            script_file.write(eval.code)
 
-    script_path=os.path.abspath(script_filename).replace('\\','\\\\')
+        script_path=os.path.abspath(script_filename).replace('\\','\\\\')
+            
+        try:
+            output = subprocess.check_output(["echo", eval.test_case_input, "|", "python", f"{script_path}"], stderr=subprocess.STDOUT, text=True, shell=True).rstrip('\n')
+            if output:
+                return {"input":eval.test_case_input,
+                        "output": output}
+            else:
+                raise HTTPException(status_code=500, detail="Error while executing code")
+            
+        except subprocess.CalledProcessError as e:
+            raise HTTPException(status_code=500, detail=str(e))
         
-    try:
-        output = subprocess.check_output(["echo", eval.test_case_input, "|", "python", f"{script_path}"], stderr=subprocess.STDOUT, text=True, shell=True).rstrip('\n')
-        if output:
-            return {"input":eval.test_case_input,
-                    "output": output}
-        else:
-            raise HTTPException(status_code=500, detail="Error while executing code")
-        
-    except subprocess.CalledProcessError as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
-    finally:
-        os.remove(script_filename)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+        finally:
+            os.remove(script_filename)
+except Exception as e:
+    print("an error occured",e)
