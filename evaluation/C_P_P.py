4b8f3020d41b15e594aeae4615ee1102c97231e5
Vbehal01 <vanshbehal01@gmail.com>
2023-09-05 16:45:12 +0530

    made a evaluation server for cpp

---
+++ evaluation/C_P_P.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+import subprocess
+import os
+from datetime import datetime
+
+def generate_unique_filename():
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    return f"dynamic_code_{current_time}.c++"
+
+#language
+class EvaluationCreate(BaseModel):
+    code: str
+    test_case_input: str
+    
+    class Config:
+        orm_mode = True
+
+app=FastAPI()
+
+try:
+    @app.post("/evaluation/CPP")
+    def evaluation(eval: EvaluationCreate):
+        script_filename = f"{generate_unique_filename()}"
+        with open(script_filename, 'w') as script_file:
+            script_file.write(eval.code)
+        
+        try:
+            subprocess.run(["g++", f"{script_filename}", "-o", "my_program"])
+            output=subprocess.run(["./my_program"],input=eval.test_case_input.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=False)
+            if output:
+                return {"input":eval.test_case_input,
+                        "output": output.stdout,
+                        "error": output.stderr}
+            else:
+                raise HTTPException(status_code=500, detail="Error while executing code")
+            
+        except subprocess.CalledProcessError as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        
+        finally:
+            os.remove(script_filename)
+except Exception as e:
+    print("an error occured",e)
