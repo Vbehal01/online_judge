a53f768eaad2e4d0d117788c8b5aeafe9cb2f393
Vbehal01 <vanshbehal01@gmail.com>
2023-09-06 12:26:02 +0530

    change of status code while returning error

--- evaluation/CPP.py
+++ evaluation/CPP.py
@@ -16,12 +16,12 @@ def evaluation(eval: schema.EvaluationCreate):
     try:
         compile_process=subprocess.run(["g++", f"{script_filename}", "-o",f"{exe_filename}"], stderr=subprocess.PIPE)
         if compile_process.returncode != 0:
-            return {"compilation_error": compile_process.stderr}
+            raise HTTPException(status_code=500, detail=f"Compilation_error: {compile_process.stderr}")
         
         else:
             output=subprocess.run([f"./{exe_filename}"],input=eval.test_case_input.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=False)
             if output.returncode != 0:
-                return {"error": "Runtime Error", "output": output.stderr}
+                raise HTTPException(status_code=500, detail=f"Runtime_error: {output.stderr}")
             else:
                 os.remove(exe_filename)
                 return {"input":eval.test_case_input,
